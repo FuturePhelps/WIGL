42990e07ddc11b5790b71290fbe7166d05267aed
Felix Bilodeau-Chagnon <felbilodeau@gmail.com>
2022-01-11 06:37:08 -0500

    moved context creation to window procedure function

--- src/application.cpp
+++ src/application.cpp
@@ -1,5 +1,6 @@
 // Includes
 #include "window.h"
+#include <gl/GL.h>
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
@@ -31,12 +32,62 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch(msg)
     {
+        // On window deletion, delete OpenGL context and exit application
         case WM_DESTROY:
-            HGLRC currentContext = wglGetCurrentContext();  // Get the current OpenGL context
-            wglMakeCurrent(GetDC(hWnd), NULL);              // Unset the OpenGL context
-            wglDeleteContext(currentContext);               // Deleting the current OpenGL context
+        {
+            wglDeleteContext(wglGetCurrentContext());       // Deleting the current OpenGL context
             PostQuitMessage(0);                             // Make sure the thread terminates
             return 0;                                       // Return with no errors
+        }
+
+        // On window creation, initialize OpenGL context
+        case WM_CREATE:
+        {
+            // Create a Pixel Format Descriptor for the device context
+            PIXELFORMATDESCRIPTOR pfd =
+            {
+                sizeof(PIXELFORMATDESCRIPTOR),                                  // Size (bytes)
+                1,                                                              // Version
+                PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,     // Flags
+                PFD_TYPE_RGBA,                                                  // Framebuffer type, RGBA or palette.
+                32,                                                             // Colordepth of the framebuffer.
+                0, 0, 0, 0, 0, 0,                                               // RedBits, RedShift, GreenBits, GreenShift, BlueBits, BlueShift
+                0,                                                              // AlphaBits
+                0,                                                              // AlphaShift
+                0,                                                              // Number of bitplanes in the accumulation buffer
+                0, 0, 0, 0,                                                     // Number of bitplanes in each RGBA accumulation buffer
+                24,                                                             // Number of bits for the depthbuffer
+                8,                                                              // Number of bits for the stencilbuffer
+                0,                                                              // Number of Aux buffers in the framebuffer.
+                PFD_MAIN_PLANE,                                                 // LayerType (deprecated)
+                0,                                                              // Number of overlay and underlay planes
+                0, 0, 0                                                         // LayerMask (deprecated), VisibleMask (transparent RGB value), DamageMask (deprecated)
+            };
+
+            // Get the device context from the window handle
+            HDC hdc = GetDC(hWnd);
+            
+            // Find the best pixel format given the parameters of pfd
+            int pixelFormat = ChoosePixelFormat(hdc, &pfd);
+
+            // Set the pixel format to the window's device context
+            if(!SetPixelFormat(hdc, pixelFormat, &pfd))
+            {
+                // If it fails, display a message box and return FALSE
+                MessageBox(NULL, "Failed to set pixel format", "Error", MB_OK);
+                return FALSE;
+            }
+
+            // Get the OpenGL context
+            HGLRC context = wglCreateContext(hdc);
+
+            // Make the context current
+            wglMakeCurrent(hdc, context);
+            break;
+        }
+
+        default:
+            return DefWindowProc(hWnd, msg, wParam, lParam);
     }
     return DefWindowProc(hWnd, msg, wParam, lParam);
 }
--- src/window.cpp
+++ src/window.cpp
@@ -17,28 +17,6 @@ int CreateApplicationWindow(WNDPROC WndProc, HINSTANCE hAppInstance, LPSTR Class
     wc.lpszMenuName = NULL;                             // Menu window name
     wc.lpszClassName = ClassName;                       // The class name under which this window class is registered
 
-
-    // Create a Pixel Format Descriptor for the device context
-    PIXELFORMATDESCRIPTOR pfd =
-    {
-        sizeof(PIXELFORMATDESCRIPTOR),                                  // Size (bytes)
-        1,                                                              // Version
-        PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,     // Flags
-        PFD_TYPE_RGBA,                                                  // Framebuffer type, RGBA or palette.
-        32,                                                             // Colordepth of the framebuffer.
-        0, 0, 0, 0, 0, 0,                                               // RedBits, RedShift, GreenBits, GreenShift, BlueBits, BlueShift
-        0,                                                              // AlphaBits
-        0,                                                              // AlphaShift
-        0,                                                              // Number of bitplanes in the accumulation buffer
-        0, 0, 0, 0,                                                     // Number of bitplanes in each RGBA accumulation buffer
-        24,                                                             // Number of bits for the depthbuffer
-        8,                                                              // Number of bits for the stencilbuffer
-        0,                                                              // Number of Aux buffers in the framebuffer.
-        PFD_MAIN_PLANE,                                                 // LayerType (deprecated)
-        0,                                                              // Number of overlay and underlay planes
-        0, 0, 0                                                         // LayerMask (deprecated), VisibleMask (transparent RGB value), DamageMask (deprecated)
-    };
-
     // Register the window class to the OS
     if(!RegisterClass(&wc))
     {
@@ -60,27 +38,6 @@ int CreateApplicationWindow(WNDPROC WndProc, HINSTANCE hAppInstance, LPSTR Class
                         hAppInstance,                                               // Handle to the application instance
                         NULL                                                        // Pointer to a value to be passed down to the window
                         );
-
-
-    // Get the device context from the window handle
-    HDC hdc = GetDC(hWnd);
-    
-    // Find the best pixel format given the parameters of pfd
-    int pixelFormat = ChoosePixelFormat(hdc, &pfd);
-
-    // Set the pixel format to the window's device context
-    if(!SetPixelFormat(hdc, pixelFormat, &pfd))
-    {
-        // If it fails, display a message box and return FALSE
-        MessageBox(NULL, "Failed to set pixel format", "Error", MB_OK);
-        return FALSE;
-    }
-
-    // Get the OpenGL context
-    HGLRC context = wglCreateContext(hdc);
-
-    // Make the context current
-    wglMakeCurrent(hdc, context);
     
     // Show and update the window before returning
     ShowWindow(hWnd, CmdShow);
